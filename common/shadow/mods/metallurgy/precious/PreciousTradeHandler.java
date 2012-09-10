520e392832dad786f7521e33da1f2559adab1007
Keith Manning <rebelkeithy@gmail.com>
2012-09-09 19:21:32 -0500

    Added more villager trades

--- common/shadow/mods/metallurgy/precious/PreciousTradeHandler.java
+++ common/shadow/mods/metallurgy/precious/PreciousTradeHandler.java
@@ -16,17 +16,34 @@ public class PreciousTradeHandler implements IVillageTradeHandler {
 	public void manipulateTradesForVillager(EntityVillager villager, MerchantRecipeList recipeList, Random random) {
 		if(villager.getProfession() == 0) // Farmer
 		{
-			
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.appleRed, 8)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.bread, 4)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.chickenCooked, 8)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.cookie, 10)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.melon, 8)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.arrow, 5)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 27, 0), null, new ItemStack(Item.flintAndSteel, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 27, 0), null, new ItemStack(Item.shears, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.chickenRaw, 17), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.wheat, 17), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.fishRaw, 12), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Block.cloth, 21), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
 		}
 		else if(villager.getProfession() == 1) // Librarian
 		{
-			
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Block.glass, 5)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 27, 0), null, new ItemStack(Block.bookShelf, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 10, 1), null, new ItemStack(Item.compass, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 10, 1), null, new ItemStack(Item.pocketSundial, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.paper, 29), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.book, 14), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.writableBook, 1), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
 		}
 		else if(villager.getProfession() == 2) // priest
 		{
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 1), null, new ItemStack(Item.expBottle, 4)));
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 1), null, new ItemStack(Item.redstone, 4)));
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 1), null, new ItemStack(Block.glowStone, 3)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.expBottle, 4)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.redstone, 4)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Block.glowStone, 3)));
 			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 10, 1), null, new ItemStack(Item.eyeOfEnder, 1)));
 		}
 		if(villager.getProfession() == 3) // Blacksmith
@@ -35,7 +52,16 @@ public class PreciousTradeHandler implements IVillageTradeHandler {
 		}
 		if(villager.getProfession() == 4) // Butcher
 		{
-			
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.beefCooked, 7)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0), null, new ItemStack(Item.porkCooked, 7)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 27, 0), null, new ItemStack(Item.helmetLeather, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 27, 0), null, new ItemStack(Item.plateLeather, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 27, 0), null, new ItemStack(Item.legsLeather, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 27, 0), null, new ItemStack(Item.bootsLeather, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(mod_MetallurgyPrecious.Coin, 64, 0), null, new ItemStack(Item.saddle, 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.beefRaw, 17), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.porkRaw, 17), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Item.coal, 23), null, new ItemStack(mod_MetallurgyPrecious.Coin, 9, 0)));
 		}
 	}
 
